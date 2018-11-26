83b4e80d81d34b10e4aad159d4710a4f76d18797
Øivind Heggland <oivind@kdrstavanger.no>
2018-11-26 16:02:08 +0100

    -- Added ARX part og the Project. -- started

---
+++ ARXToKDRGold/ARXExporter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ARXToKDRGold
+{
+    public partial class ARXExporter : ServiceBase
+    {
+        public ARXExporter()
+        {
+            InitializeComponent();
+        }
+
+        private Timer timer1 = null;
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer();
+            this.timer1.Interval = 30000;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            Library.WriteErrorLog("Test window service started");
+        }
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            // write code her to do the job based on my requirements
+            //GET DATA FROM ARX WEB REWUEST AUTH BASIC
+
+            Library.WriteErrorLog("Timer ticked and som job as been done successfully");
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Library.WriteErrorLog("Test window service stopped");
+        }
+    }
+}
---
+++ ARXToKDRGold/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARXToKDRGold
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            LogMSG(ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            LogMSG(Message);
+        }
+
+        private static void LogMSG(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
